ed06d89c39a936098abe464b58bd0bbbdbf297cd
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-11-30 08:57:44 -0800

    did some styling on the form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 import axios from 'axios';
 
 const CreateAccount = (props) => {
     const [user, setUser] = useState({
+        firstname:"",
+        lastname:"",
+        email:"",
         username:"",
         password:""
     });
@@ -21,49 +25,102 @@ const CreateAccount = (props) => {
     }
 
     return (
-        <div>
-            <form onSubmit={submitForm}>
-                <div>
-                    <h2>Create Account</h2>
-                    <label>First Name: </label>
-                    <input
-                        type="text"
-                        name="username"
-                        placeholder="enter Username"
-                        onChange={handleChanges}
-                        value={user.username} />
-                    <label>Last Name: </label>
-                    <input
-                        type="text"
-                        name="username"
-                        placeholder="enter Username"
-                        onChange={handleChanges}
-                        value={user.username} />
-                    <label>Email</label>
-                    <input
-                        type="text"
-                        name="username"
-                        placeholder="enter Username"
-                        onChange={handleChanges}
-                        value={user.username} />
-                    <label>Username</label>
-                    <input
-                        type="text"
-                        name="username"
-                        placeholder="enter Username"
-                        onChange={handleChanges}
-                        value={user.username} />
-                    <label>Password</label>
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Enter Password"
-                        onChange={handleChanges}
-                        value={user.password} />
-                    </div>
+        
+            <StyledForm onSubmit={submitForm}>
+                    <h2>Create Account</h2>         
+                    <StyledInputContainer>
+                        <label>First Name </label>
+                        <input
+                            type="text"
+                            name="firstname"
+                            placeholder="Enter first name"
+                            onChange={handleChanges}
+                            value={user.firstname} />
+                    </StyledInputContainer>
+                    <StyledInputContainer>
+                        <label>Last Name </label>
+                        <input
+                            type="text"
+                            name="lastname"
+                            placeholder="Enter last name"
+                            onChange={handleChanges}
+                            value={user.lastname} />
+                    </StyledInputContainer>    
+                    <StyledInputContainer>
+                        <label>Email</label>
+                        <input
+                            type="text"
+                            name="email"
+                            placeholder="Enter your email"
+                            onChange={handleChanges}
+                            value={user.email} />
+                    </StyledInputContainer>    
+                    <StyledInputContainer>
+                        <label>Username</label>
+                        <input
+                            type="text"
+                            name="username"
+                            placeholder="Enter a username"
+                            onChange={handleChanges}
+                            value={user.username} />
+                    </StyledInputContainer>
+                    <StyledInputContainer>
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Enter a password"
+                            onChange={handleChanges}
+                            value={user.password} />
+                    </StyledInputContainer>  
                     <button>Create Account</button>
-            </form>
-        </div>
+            </StyledForm>
+        
     )
     }
 export default CreateAccount;
+
+const StyledFormContainer = styled.div`
+
+`
+
+const StyledForm = styled.form`
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+    width:380px;
+    border:1px solid black;
+    border-radius:10px;
+    z-index:1;
+    input{
+        width:300px;
+        height:30px;
+        font-size:1.2rem;
+        border-radius:7px;
+    }
+    button{
+        width:300px;
+        background-color:teal;
+        border-radius:7px;
+        font-size:1.2rem;
+        height:30px;
+        color:white;
+        margin:20px;
+    }
+    label{
+        font-size:.8rem;
+        color:grey;
+    }
+    h2{
+        text-align:center;
+        font-size:1.4rem;
+        margin-top:20px;
+        margin-bottom:5px;
+    }
+`
+const StyledInputContainer=styled.div`
+    display:flex;
+    flex-direction:column;
+    margin:5px;
+`
