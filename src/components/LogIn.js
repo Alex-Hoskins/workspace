8a0eb63f2a281ee41a44468c2b979c81b0be1fee
Alex Hoskins <86258010+Alex-Hoskins@users.noreply.github.com>
2022-06-02 14:19:50 -0700

    Landing page (#13)
    * Changed landing page styles
    
    * Made landing page more responsive
    
    * updated readme
    
    * Styled Login and Signup to be consistent with landingpage
    
    * Fixed responsiveness of forms

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -126,7 +126,7 @@ const StyledForm = styled.form`
     justify-content:center;
     align-items:center;
     width:380px;
-    height:380px;
+    min-height:400px;
     border-radius:10px;
     background-color:white;
     input{
